595ae9fadf9fc3e301e927e11b0c08855d144014
jeanoza <choikb916@gmail.com>
2021-12-13 21:08:42 +0100

    feat(until d flag)

--- ft_printf.c
+++ ft_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kychoi <kychoi@student.42.fr>              +#+  +:+       +#+        */
+/*   By: kyubongchoi <kyubongchoi@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 14:17:15 by kychoi            #+#    #+#             */
-/*   Updated: 2021/12/10 17:44:09 by kychoi           ###   ########.fr       */
+/*   Updated: 2021/12/13 21:08:28 by kyubongchoi      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,65 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
-#include <stdio.h>
+#include <stdio.h> //**TO_REMOVE
 
-int	ft_print_char(va_list ap, int	*index)
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str && str[i])
+		++i;
+	return (i);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb, int *count)
+{
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb > 9)
+		ft_putnbr(nb / 10, count);
+	ft_putchar((nb % 10) + 48);
+	*count += 1;
+}
+int	ft_print_int(va_list ap)
+{
+	int	nb;
+	int	count;
+
+	nb = va_arg(ap, int);
+	count = 0;
+	if (ap)
+		ft_putnbr(nb, &count);
+	return (count);
+}
+
+int	ft_print_char(va_list ap)
 {
 	char	c;
 
 	c = va_arg(ap, int);
 	if (ap)
-	{
-		write(1, &c, 1);
-		*index += 1;
-	}
-	return (1);
+		return (write(1, &c, 1));
+	return (0);
+}
+
+int	ft_print_str(va_list ap)
+{
+	char	*str;
+
+	str = va_arg(ap, char *);
+	if (ap)
+		return (write(1, str, ft_strlen(str)));
+	return (0);
 }
 
 int	ft_printf(const char *format, ...)
@@ -42,15 +88,21 @@ int	ft_printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
-			if (format[i + 1] == 'c')
-				res += ft_print_char(ap, &i);
+			++i;
+			if (format[i] == 'c')
+				res += ft_print_char(ap);
+			if (format[i] == 's')
+				res += ft_print_str(ap);
+			if (format[i] == 'd')
+				res += ft_print_int(ap);
+				
 		}
 		else
 			write(1, &format[i], 1);
 		++i;
 	}
 	va_end(ap);
-	return (0);
+	return (res);
 }
 
 
@@ -61,20 +113,22 @@ int	main(int ac, char **av)
 
 	//mine
 	printf("\nft_printf:\n");
-	ft_printf("c test	:%c\n", *(av[1]));
+	printf("	(return:%d)\n", ft_printf("c	:%c", *(av[1])));
+	printf("	(return:%d)\n", ft_printf("s	:%s", (av[1])));
+	printf("	(return:%d)\n", ft_printf("d	:%d", 2147483647));
 
 	//char....
 	printf("\nprintf:\n");
 	printf("	(return:%d)\n", printf("c	:%c", *(av[1])));
 	printf("	(return:%d)\n", printf("s	:%s", (av[1])));
-	printf("	(return:%d)\n", printf("p	:%p", (av[1])));
 	//return min 4(when 1 char, 3 + 1), %p return 17(3 + 14(2(0x)+ 12))
 
 	//numbers...
 	// printf("sizeof	:%lu\n", sizeof atoi(av[1]));// sizeof int = 4
-	printf("	(return:%d)\n", printf("d	:%d", atoi(av[1])));
+	printf("	(return:%d)\n", printf("d	:%d", 2147483647));
 	printf("	(return:%d)\n", printf("i	:%i", atoi(av[1]))); // base 10(decimal)
 	printf("	(return:%d)\n", printf("u	:%u", atoi(av[1]))); // unsigned int
+	printf("	(return:%d)\n", printf("p	:%p", (av[1])));
 	printf("	(return:%d)\n", printf("x	:%x", atoi(av[1]))); 
 	printf("	(return:%d)\n", printf("X	:%X", atoi(av[1]))); 
 	printf("	(return:%d)\n", printf("%%	:%d%%", atoi(av[1]))); 
